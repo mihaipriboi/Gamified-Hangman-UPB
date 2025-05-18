96e3b9f8a05425c97883ed9f3b877ec59fe4c037
mihai <mihailucapriboi@gmail.com>
2025-05-19 00:28:19 +0300

    delete large files

--- lib/SDL2-2.32.6/build/SDL_atomic.d
+++
@@ -1,8 +0,0 @@
-build/SDL_atomic.lo: \
- /home/mihai/Documents/Info/ACS_PA/Proiect/lib/SDL2-2.32.6/src/atomic/SDL_atomic.c \
- /home/mihai/Documents/Info/ACS_PA/Proiect/lib/SDL2-2.32.6/src/atomic/../SDL_internal.h \
- /home/mihai/Documents/Info/ACS_PA/Proiect/lib/SDL2-2.32.6/src/atomic/../dynapi/SDL_dynapi.h \
- /home/mihai/Documents/Info/ACS_PA/Proiect/lib/SDL2-2.32.6/src/atomic/../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h include/SDL_assert.h include/SDL_stdinc.h \
- include/SDL_config.h include/SDL_log.h include/SDL_atomic.h
--- lib/SDL2-2.32.6/build/SDL_sandbox.d
+++
@@ -1,9 +0,0 @@
-build/SDL_sandbox.lo: \
- /home/mihai/Documents/Info/ACS_PA/Proiect/lib/SDL2-2.32.6/src/core/linux/SDL_sandbox.c \
- /home/mihai/Documents/Info/ACS_PA/Proiect/lib/SDL2-2.32.6/src/core/linux/../../SDL_internal.h \
- /home/mihai/Documents/Info/ACS_PA/Proiect/lib/SDL2-2.32.6/src/core/linux/../../dynapi/SDL_dynapi.h \
- /home/mihai/Documents/Info/ACS_PA/Proiect/lib/SDL2-2.32.6/src/core/linux/../../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h include/SDL_platform.h include/begin_code.h \
- include/close_code.h include/SDL_assert.h include/SDL_stdinc.h \
- include/SDL_config.h include/SDL_log.h \
- /home/mihai/Documents/Info/ACS_PA/Proiect/lib/SDL2-2.32.6/src/core/linux/SDL_sandbox.h
