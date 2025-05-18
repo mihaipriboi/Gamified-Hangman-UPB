b4525d122d6b4f0cd03d176eebb2d778b664d8d1
mihai <mihailucapriboi@gmail.com>
2025-05-19 00:37:26 +0300

    try fix win bat

--- build_run_windows.bat
+++ build_run_windows.bat
@@ -1,33 +1,42 @@
 @echo off
-REM === Compilator, foldere si fisiere ===
+REM === Compiler, folders and files ===
 set CC=gcc
 set SRCDIR=src
 set OBJDIR=build
 set BINARY=%OBJDIR%\hangman.exe
 
-REM === Include si lib ===
+REM === Includes and libs ===
 set CFLAGS=-Wall -Wextra -Werror -Iinclude
-set LDFLAGS=-Llib\windows -lSDL2 -lSDL2_ttf
+set LDFLAGS=-Llib\windows -lSDL2 -lSDL2_ttf -lmingw32 -lm
 
-REM === Creeaza folderul de build daca nu exista ===
+REM === Create build dir if not exists ===
 if not exist %OBJDIR% (
     mkdir %OBJDIR%
 )
 
-REM === Compilare ===
+REM === Compile ===
 echo Compiling...
 %CC% %CFLAGS% %SRCDIR%\*.c -o %BINARY% %LDFLAGS%
 if errorlevel 1 (
     echo Eroare la compilare!
+    pause
     exit /b 1
 )
 
-REM === Copierea bibliotecilor ===
+if not exist %BINARY% (
+    echo Compilation failed: binary not found.
+    pause
+    exit /b 1
+)
+
+REM === Copy DLLs ===
 copy /Y lib\windows\SDL2.dll %OBJDIR%\ >nul
 copy /Y lib\windows\SDL2_ttf.dll %OBJDIR%\ >nul
 
-REM === Rulare ===
+REM === Run ===
 echo Running...
 cd %OBJDIR%
 hangman.exe
+echo.
+pause
 cd ..
